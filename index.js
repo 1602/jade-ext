59d899ed2103a05996da345f0a3519950db43f20
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-01-11 02:39:11 +0400

    Update jade to work with railway 0.2.6

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+var fs = require('fs');
+
 // monkey patch ejs
-var jade = require('jade'), old_parse = jade.Compiler.prototype.compile;
-jade.Compiler.prototype.compile = function () {
-    var str = old_parse.apply(this, Array.prototype.slice.call(arguments));
-    // console.log(str);
-    return 'arguments.callee.buf = buf;' + str;
-};
+try {
+    var jade = require('jade'), old_parse = jade.Compiler.prototype.compile;
+    jade.Compiler.prototype.compile = function () {
+        var str = old_parse.apply(this, Array.prototype.slice.call(arguments));
+        // console.log(str);
+        return 'arguments.callee.buf = buf;' + str;
+    };
+} catch (e) {
+    // disregard
+}
 
 /**
  * This extension will be used by default for all template files
@@ -28,6 +34,17 @@ exports.templateText = function (name, data) {
 
         case 'default_action_view':
         return '.page-header\n  h1 ' + data.join('#') + '\n';
+
+        case 'scaffold_show':
+        var fields = [];
+        data.forEach(function (property) {
+            switch (property.type) {
+                default:
+                fields.push('.field(property=\'' + property.name + '\')= model.' + property.name);
+                break;
+            }
+        });
+        return fs.readFileSync(exports.template('scaffold_show')).toString().replace('FIELDS', fields.join('\n  '));
 
         case 'scaffold_form':
         var form = '';
